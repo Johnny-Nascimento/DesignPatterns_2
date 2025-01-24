7c4713b1f2ee7fb9d41d257f1ab1199e71663c91
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-01-24 07:32:33 -0300

    - Separado arquivos de design.

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -1,32 +1,12 @@
 ﻿using Microsoft.Data.SqlClient;
 using System.Data;
 
-namespace MyApp
+namespace MyApp_Memento
 {
-    public class ConnectionFactory
-    {
-        public IDbConnection GetConnection()
-        {
-            IDbConnection conexao = new SqlConnection();
-            conexao.ConnectionString = "User Id=root;Password=;Server=localhost;Database=banco";
-            conexao.Open();
-
-            return conexao;
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            IDbConnection conexao = new ConnectionFactory().GetConnection();
-
-            using (IDbCommand comando = conexao.CreateCommand())
-            {
-                comando.CommandText = "select * from tabela";
-            }
-
-            conexao.Close();
         }
     }
 }
